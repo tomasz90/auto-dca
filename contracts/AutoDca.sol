dd5e58a74860acc3f3f95ca3cc32853525e3af64
tomasz <tomaszkacperkulig@gmail.com>
2022-05-21 21:14:18 +0200

    rough sketch

--- contracts/AutoDca.sol
+++ contracts/AutoDca.sol
@@ -10,41 +10,34 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
-contract AutoDca is KeeperCompatibleInterface, Pausable, Ownable {
+contract AutoDca is KeeperCompatibleInterface, Ownable {
 
     uint256 public counter;
-    uint256 public lastTimeStamp;
-    uint256 public interval;
-
-    uint256 public amount;
-
-    IERC20 public stableToken;
-    IERC20 public dcaIntoToken;
 
     IUniswapV3Factory uniswapFactory;
 
     address public keeperRegistryAddress;
 
+    mapping(address => AccountParams) public accountsParams;
+    address[] public accounts;
+
     event KeeperRegistryAddressUpdated(address oldAddress, address newAddress);
     event AmountUpdated(uint256 oldAmount, uint256 newAmount);
 
+    struct AccountParams {
+        uint256 interval;
+        uint256 actionTime;
+        uint256 amount;
+        IERC20 stableToken;
+        IERC20 dcaIntoToken;
+        bool paused;
+    }
+
     constructor(
-        uint256 _interval,
-        uint256 _amount,
-        IERC20 _stableToken,
-        IERC20 _dcaIntoToken,
         IUniswapV3Factory _uniswapFactory,
         address _keeperRegistryAddress
     ) {
-        interval = _interval;
-        lastTimeStamp = block.timestamp;
         counter = 0;
-
-        amount = _amount;
-
-        stableToken = _stableToken;
-        dcaIntoToken = _dcaIntoToken;
-
         uniswapFactory = _uniswapFactory;
         keeperRegistryAddress = _keeperRegistryAddress;
     }
@@ -59,35 +52,23 @@ contract AutoDca is KeeperCompatibleInterface, Pausable, Ownable {
         view
         override
         returns (bool upkeepNeeded, bytes memory performData)
-    {
-        if(paused()) {
-            return(false, performData);
+    {   
+
+        for(uint i; i < accounts.length; i++) {
+            AccountParams memory account = accountsParams[accounts[i]];
+            uint256 actionTime = account.actionTime;
+            bool spendable = spendable(accounts[i], account.stableToken, account.amount);
+            if(actionTime < block.timestamp && spendable) {
+                upkeepNeeded = true;
+                performData = abi.encode(accounts[i]);
+            }
         }
-
-        uint256 balance = stableToken.balanceOf(address(this));
-
-        if(balance < amount) {
-            return(false, performData);
-        }
-
-        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
     }
 
-    function performUpkeep(bytes calldata performData) external override onlyKeeperRegistry whenNotPaused {
+    function performUpkeep(bytes calldata performData) external override onlyKeeperRegistry {
         counter++;
-        lastTimeStamp = block.timestamp;
-        
-        uint256 balance = stableToken.balanceOf(address(this));
-        require(balance >= amount, "Not enough funds");
-
-        IUniswapV3Pool pool = findPool();
-
-        stableToken.approve(address(pool), amount);
-
-        bool zeroForOne = address(stableToken) < address(dcaIntoToken);
-
-        pool.swap(address(this), zeroForOne, int256(amount), 0, "");
-
+        address user = abi.decode(performData, (address));
+        swap(user, accountsParams[user]);
     }
 
     function setKeeperRegistryAddress(address _keeperRegistryAddress) external onlyOwner {
@@ -95,37 +76,39 @@ contract AutoDca is KeeperCompatibleInterface, Pausable, Ownable {
         keeperRegistryAddress = _keeperRegistryAddress;
     }
 
-    function setAmount(uint256 _amount) external onlyOwner {
-        emit AmountUpdated(amount, _amount);
-        amount = _amount;
+    function setUpAccount(uint256 _interval, uint256 _amount, IERC20 _stableToken, IERC20 _dcaIntoToken) external {
+        AccountParams memory params = AccountParams(
+            _interval,
+            block.timestamp + _interval,
+            _amount,
+            _stableToken,
+            _dcaIntoToken,
+            false
+        );
+        accountsParams[msg.sender] = params;
+        accounts.push(msg.sender);
     }
 
-    function setInterval(uint256 _interval) external onlyOwner {
-        interval = _interval;
+    function swap(address user, AccountParams memory params) private {
+        uint256 balance = params.stableToken.balanceOf(address(this));
+        require(balance >= params.amount, "Not enough funds");
+
+        IUniswapV3Pool pool = findPool(user, params.stableToken, params.dcaIntoToken);
+
+        params.stableToken.approve(address(pool), params.amount);
+
+        bool zeroForOne = address(params.stableToken) < address(params.dcaIntoToken);
+
+        pool.swap(address(this), zeroForOne, int256(params.amount), 0, "");
     }
 
-    function setStableToken(IERC20 _token) public onlyOwner {
-        stableToken = _token;
+    function spendable(address user, IERC20 stableToken, uint256 amount) private view returns(bool) {
+        uint256 allowance = stableToken.allowance(user, address(this));
+        return stableToken.balanceOf(user) > amount
+            && allowance > amount;
     }
 
-    function setDcaIntoToken(IERC20 _token) public onlyOwner {
-        dcaIntoToken = _token;
-    }
-
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
-    }
-
-    function withdraw(address token) external onlyOwner {
-        uint256 balance = IERC20(token).balanceOf(address(this));
-        IERC20(token).transfer(owner(), balance);
-    }
-
-    function findPool() private returns (IUniswapV3Pool) {
+    function findPool(address user, IERC20 stableToken, IERC20 dcaIntoToken) private returns (IUniswapV3Pool) {
         uint24[3] memory fee = [uint24(100), uint24(500), uint24(3000)];
         for (uint256 i; i < fee.length; i++) {
             address poolAddress = uniswapFactory.getPool(
@@ -138,7 +121,8 @@ contract AutoDca is KeeperCompatibleInterface, Pausable, Ownable {
             }
         }
 
-        _pause(); // pause upkeep, there is no sense to perform upkeeps when swap cannot be done
+         // there is no sense to perform upkeeps when swap cannot be done
+        accountsParams[user].paused = true;
 
         string memory message = string(
             abi.encodePacked("No pool with tokens: ", 
