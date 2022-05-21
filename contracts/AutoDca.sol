2bf2d6ad84876520394cb7c9948fd5c397b4b813
tomasz <tomaszkacperkulig@gmail.com>
2022-05-21 11:21:10 +0200

    needs tests

--- contracts/AutoDca.sol
+++ contracts/AutoDca.sol
@@ -2,30 +2,50 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import "@aave/periphery-v3/contracts/rewards/interfaces/IRewardsController.sol";
-import "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
-import "@aave/core-v3/contracts/interfaces/IPool.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 contract AutoDca is KeeperCompatibleInterface, Ownable {
     uint256 public counter;
     uint256 public immutable interval;
     uint256 public lastTimeStamp;
-    IRewardsController public immutable rewardsController;
-    IPoolAddressesProvider public immutable poolAddressesProvider;
-    IUniswapV3Pool public immutable pool;
+
+    uint256 public amount;
+
+    IERC20 public immutable stableToken;
+    IERC20 public immutable dcaIntoToken;
+
+    uint24 public poolFee;
+
+    ISwapRouter uniswapRouter;
+
+    address public keeperRegistryAddress;
+
+    event KeeperRegistryAddressUpdated(address oldAddress, address newAddress);
+    event AmountUpdated(uint256 oldAmount, uint256 newAmount);
 
     constructor(
         uint256 _interval,
-        address _poolAddressProviderAddress,
-        address _rewardsControllerAddress,
-        address _uniswapV3Pool
+        uint256 _amount,
+        IERC20 _stableToken,
+        IERC20 _dcaIntoToken,
+        IUniswapV3Factory _uniswapFactory,
+        ISwapRouter _uniswapRouter
     ) {
         interval = _interval;
         lastTimeStamp = block.timestamp;
         counter = 0;
-        rewardsController = IRewardsController(_rewardsControllerAddress);
-        pool = IUniswapV3Pool(_uniswapV3Pool);
+        amount = _amount;
+        stableToken = _stableToken;
+        dcaIntoToken = _dcaIntoToken;
+        poolFee = findPoolFee(_uniswapFactory, _stableToken, _dcaIntoToken);
+        uniswapRouter = _uniswapRouter;
+    }
+
+    modifier onlyKeeperRegistry() {
+        require(msg.sender == keeperRegistryAddress, "Caller is not the keeper registry");
+        _;
     }
 
     function checkUpkeep(bytes calldata checkData)
@@ -37,11 +57,50 @@ contract AutoDca is KeeperCompatibleInterface, Ownable {
         upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
     }
 
-    function performUpkeep(bytes calldata performData) external override {
-        IPool aavePool = poolAddressesProvider.getPool();
-        address[] memory reserves = aavePool.getReservesList();
-        (address[] memory rewardsList, uint256[] memory claimedAmounts)
-         = rewardsController.claimAllRewardsOnBehalf(reserves, owner(), owner());
-         
-        pool.swap(owner(), )
+    function performUpkeep(bytes calldata performData) external onlyKeeperRegistry override {
+        stableToken.transferFrom(owner(), address(this), amount);
+        stableToken.approve(address(uniswapRouter), amount);
+
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(stableToken),
+                tokenOut: address(dcaIntoToken),
+                fee: poolFee,
+                recipient: owner(),
+                deadline: block.timestamp + 60,
+                amountIn: amount,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        uniswapRouter.exactInputSingle(params);
+    }
+
+    function findPoolFee(
+        IUniswapV3Factory _uniswapFactory,
+        IERC20 _stableToken,
+        IERC20 _dcaIntoToken
+    ) private view returns (uint24) {
+        uint24[3] memory fee = [uint24(100), uint24(500), uint24(3000)];
+        for (uint256 i; i < fee.length; i++) {
+            address poolAddress = _uniswapFactory.getPool(
+                address(_stableToken),
+                address(_dcaIntoToken),
+                fee[i]
+            );
+            if (poolAddress != address(0)) {
+                return fee[i];
+            }
+        }
+    }
+
+    function setKeeperRegistryAddress(address _keeperRegistryAddress) external onlyOwner {
+        emit KeeperRegistryAddressUpdated(keeperRegistryAddress, _keeperRegistryAddress);
+        keeperRegistryAddress = _keeperRegistryAddress;
+  }
+
+  function setAmount(uint _amount) external onlyOwner {
+        emit AmountUpdated(amount, _amount);
+        amount = _amount;
+  }
 }
