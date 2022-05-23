f5cecc770c13c5a066d245da4ffa07cf8de2ab32
tomasz <tomaszkacperkulig@gmail.com>
2022-05-23 17:28:52 +0200

    formatting fixed

--- truffle-config.js
+++ truffle-config.js
@@ -1,48 +1,51 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    // See <http://truffleframework.com/docs/advanced/configuration>
-    // to customize your Truffle configuration!
-    networks: {
-        development: {
-            host: "127.0.0.1", // Localhost (default: none)
-            port: 7548, // Standard Ethereum port (default: none)
-            network_id: 10001, // Any network (default: none)
-            gas: 600000000,
-        },
-        mumbai: {
-            provider: () => new HDWalletProvider(process.env.MNEMONIC, 
-            "https://polygon-mumbai.infura.io/v3/09f56f531b7c4afb9fcdf5c711ecf700"),
-            network_id: 80001,
-            confirmations: 2,
-            timeoutBlocks: 1000,
-            skipDryRun: true,
-            gas: 5000000,
-            gasPrice: 20000000000,
-        },
-        rinkeby: {
-            provider: () => new HDWalletProvider(process.env.MNEMONIC, 
-            "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"),
-            network_id: 4,
-            confirmations: 2,
-            timeoutBlocks: 1000,
-            skipDryRun: true,
-            gas: 5000000,
-            gasPrice: 20000000000,
-        }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7548, // Standard Ethereum port (default: none)
+      network_id: 10001, // Any network (default: none)
+      gas: 600000000,
     },
-    compilers: {
-        solc: {
-            version: "0.8.6"
-        }
+    mumbai: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://polygon-mumbai.infura.io/v3/" + process.env.INFURA_API_KEY
+        ),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 1000,
+      skipDryRun: true,
+      gas: 5000000,
+      gasPrice: 20000000000,
     },
-    plugins: [
-        'truffle-plugin-verify',
-        'truffle-ganache-test'
-    ],
-    api_keys: {
-        polygonscan: process.env.POLYGON_SCAN_API_KEY,
-        etherscan: process.env.ETHERSCAN_API_KEY
-      }  
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY
+        ),
+      network_id: 4,
+      confirmations: 2,
+      timeoutBlocks: 1000,
+      skipDryRun: true,
+      gas: 5000000,
+      gasPrice: 20000000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.6",
+    },
+  },
+  plugins: ["truffle-plugin-verify", "truffle-ganache-test"],
+  api_keys: {
+    polygonscan: process.env.POLYGON_SCAN_API_KEY,
+    etherscan: process.env.ETHERSCAN_API_KEY,
+  },
 };
