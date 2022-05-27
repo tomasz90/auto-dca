f063fff2745063d3390fa570ecb5279bee99befc
tomasz <tomaszkacperkulig@gmail.com>
2022-05-27 21:19:58 +0200

    in progress

--- scripts/compile-contracts.js
+++ scripts/compile-contracts.js
@@ -1,45 +1,58 @@
-const fs = require('fs');
+const fs = require("fs");
 const path = require("path");
-const solc = require('solc');
+const solc = require("solc");
 
 module.exports = function compileAll(mainContract) {
-    let contract = mainContract + '.sol'
-    resolved = path.resolve('contracts', contract);
-    let source = fs.readFileSync(resolved, 'utf8');
+  let contract = mainContract + ".sol";
+  resolved = path.resolve("contracts", contract);
+  let source = fs.readFileSync(resolved, "utf8");
 
-    const input = {
-        language: 'Solidity',
-        sources: {
-          [contract]: {
-            content: source
-          }
+  const input = {
+    language: "Solidity",
+    sources: {
+      [contract]: {
+        content: source,
+      },
+    },
+    settings: {
+      outputSelection: {
+        "*": {
+          "*": ["*"],
         },
-        settings: {
-          outputSelection: {
-            '*': {
-              '*': ['*']
-            }
-          }
-        }
-      };
+      },
+    },
+  };
 
-      function readContractsFrom(dir, relativePath) {
-        const absolutePath = path.resolve(dir, relativePath);
-        return fs.readFileSync(absolutePath, 'utf8');
-      }
-      
-      function findImports(relativePath) {
-        let source;
-        try {
-            source = readContractsFrom('contracts', relativePath);
-        } catch(error) {
-            source = readContractsFrom('node_modules', relativePath);
-        }     
-        return { contents: source };
-      }
-      
-      var output = JSON.parse(
-        solc.compile(JSON.stringify(input), { import: findImports })
-      );
-      return output;
-}
+  function readContractsFrom(dir, relativePath) {
+    const absolutePath = path.resolve(dir, relativePath);
+    return fs.readFileSync(absolutePath, "utf8");
+  }
+
+  function findImports(relativePath) {
+    let source;
+    try {
+      source = readContractsFrom("contracts", relativePath);
+    } catch (error) {
+      source = readContractsFrom("node_modules", relativePath);
+    }
+    return { contents: source };
+  }
+
+  let output = JSON.parse(solc.compile(JSON.stringify(input), { import: findImports }));
+
+  let contracts = {};
+
+  for (const value of Object.values(output.contracts)) {
+    for (const [contractName, details] of Object.entries(value)) {
+      let bytecode = details.evm.bytecode.object;
+      let abi = details.abi;
+
+      let contract = {};
+      contract["abi"] = abi;
+      contract["bytecode"] = bytecode;
+
+      contracts[contractName] = contract;
+    }
+  }
+  return contracts;
+};
--- scripts/set-up-account.js
+++ scripts/set-up-account.js
@@ -4,9 +4,40 @@ const compileAll = require("./compile-contracts");
 require("dotenv").config();
 
 async function main() {
-    let wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
-    let output = compileAll("AutoDca");
-    console.log(output);
+
+    let contracts = compileAll("AutoDca");
+  
+    let provider = ethers.getDefaultProvider('rinkeby');
+    let wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC).connect(provider);
+
+    // deployContracts(abi, bytcode, wallet);
+
+    let autoDcaContract = new ethers.Contract("0x88955862200ac3c2d630f3c5b31072e06d75f2c4", contracts.AutoDca.abi, provider);
+
+    let managerAddress = await autoDcaContract.manager();
+
+    let managerContract = new ethers.Contract(managerAddress, contracts.AccountManager.abi, provider).connect(wallet);
+
+    await managerContract.setPause();
+
+}
+
+async function deployContracts(contracts, wallet) {
+
+    let { abi, bytecode } = contracts.AutoDca;
+
+    let factory = new ethers.ContractFactory(abi, bytecode, wallet);
+
+    // Rinkeby
+    const uniswapRouter = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+    const uniswapFactory = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
+    const ops = "0x8c089073A9594a4FB03Fa99feee3effF0e2Bc58a";
+
+    let contract = await factory.deploy(uniswapRouter, uniswapFactory, ops);
+
+    return contract.address;
 }
 
 main();
+
+
